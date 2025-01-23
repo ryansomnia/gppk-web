da119961929af4832914c47f151f2acb39887604
30003000666 <30003000666@brilife.co.id>
2025-01-23 17:40:58 +0700

    coba image naik

--- src/components/WorshipLayouts.js
+++ src/components/WorshipLayouts.js
@@ -1,5 +1,9 @@
 import React from 'react';
-// import bgImage from '../../public/images/';
+// const images = [
+//   '/images/bg1.JPG',
+//   '/images/bg2.JPG',
+//   '/images/sm1.JPG',
+// ];
 // ...
 // style={{
 //   backgroundImage: `url(${bgImage})`
@@ -11,7 +15,7 @@ const WorshipLayout = () => {
     <div
       className="text-white flex items-center justify-center bg-center h-96 md:h-[70vh] md:w-[60%] shadow-xl"
       style={{
-        backgroundImage: " linear-gradient(rgba(0, 251, 255, 0.6), rgba(0, 251, 255, 0.6)), url('../../public/images/usher1.jpg')", // Transparansi lebih rendah
+        backgroundImage: " linear-gradient(rgba(0, 251, 255, 0.6), rgba(0, 251, 255, 0.6)), url('/images/usher1.jpg')", // Transparansi lebih rendah
         backgroundSize: 'cover',
         backgroundPosition: 'center',
         transition: 'background-image 1s ease-in-out',
@@ -33,13 +37,13 @@ const WorshipLayout = () => {
       <div className="flex flex-wrap md:w-[40%]">
         {/* First Block */}
         <div className="flex items-end justify-center w-1/2 h-48 md:h-1/2" style={{
-  backgroundImage: " url('../../public/images/bg1.jpg')",
-  backgroundSize: 'cover',
+            backgroundImage: "url('/images/bg1.jpg')",
+            backgroundSize: 'cover',
   backgroundPosition: 'center',
   transition: 'background-image 1s ease-in-out',
 }}>
    <div className=" text-center bg-yellow-600 py-1 px-2 m-1 rounded-lg">
-  <h3 className="text-[10px] sm:text-xl font-semibold text-white">Ibadah Raya 1</h3>
+  <h3 className="text-[10px] sm:text-xl font-semibold text-white">Ibadah Raya Umum</h3>
   <p className="text-[8px] sm:text-xs font-normal text-white leading-tight">Ibadah Umum setiap Minggu, Jam 09:00 WIB</p>
 </div>
 </div>
@@ -47,7 +51,7 @@ const WorshipLayout = () => {
         {/* Second Block */}
         <div className=" flex items-end justify-center w-1/2 h-48 md:h-1/2"
          style={{
-          backgroundImage: "url('../../public/images/bg2.jpg')", // Transparansi lebih rendah
+          backgroundImage: "url('/images/bg2.jpg')",
   
           backgroundSize: 'cover',
           backgroundPosition: 'center',
@@ -55,14 +59,14 @@ const WorshipLayout = () => {
         }}>
           
           <div className=" text-center bg-yellow-600 py-1 px-2 m-1 rounded-lg">
-  <h3 className="text-[10px] sm:text-xl font-semibold text-white">Ibadah Raya 2</h3>
-  <p className="text-[8px] sm:text-xs font-normal text-white leading-tight">Ibadah Pemuda setiap Minggu, Jam 16:00 WIB</p>
+  <h3 className="text-[10px] sm:text-xl font-semibold text-white">Ibadah Youth</h3>
+  <p className="text-[8px] sm:text-xs font-normal text-white leading-tight">Ibadah Anak Muda setiap Minggu, Jam 16:00 WIB</p>
 </div>
         </div>
 
         {/* Third Block */}
         <div className=" text-white flex items-end justify-center w-1/2 h-48 md:h-1/2" style={{
-          backgroundImage: "url('../../public/images/sm1.jpg')", // Transparansi lebih rendah
+            backgroundImage: "url('/images/sm1.jpg')",
   
           backgroundSize: 'cover',
           backgroundPosition: 'center',
